9303a8162764e4cc3c081188348ad46ea18d4a9e
Miguel Alvarez <kamapu78@gmail.com>
2018-06-20 01:13:10 +0200

    New function 'read_sunscan'

--- R/pile.R
+++ R/pile.R
@@ -11,32 +11,22 @@ setGeneric("pile",
 
 # Method for data.frame
 setMethod("pile", signature(x="data.frame"),
-		function(x, time, ...) {
-			if(is.null(dim(time))) {
-				if(length(time) != nrow(x))
-					stop("Length of 'time' does not macht number of rows in 'x'.")
-			}
-			if(is.data.frame(time)) {
-				if(nrow(time) != nrow(x))
-					stop("Number of rows in 'time' does not macht number of rows in 'x'.")
-			}
+		function(x, time, biomass, ...) {
 			out <- new("piled_curves")
-			if(is.null(dim(time)))
-				out@time <- data.frame(time=time, stringsAsFactors=FALSE) else
-				out@time <- time
-			out@input <- as.matrix(x)
-			out@piled <- t(apply(as.matrix(x), 1, cumsum))
-			out@relative <- t(apply(as.matrix(x), 1,
-							function(x) cumsum(x)/sum(x)))
+			out@time <- x[,time]
+			out@input <- as.matrix(x[,biomass])
+			out@piled <- t(apply(out@input, 1, cumsum))
+			out@relative <- t(apply(out@input, 1, function(x) cumsum(x)/sum(x)))
 			return(out)
 		}
 )
 
-# method for matrix
-setMethod("pile", signature(x="matrix"),
-		function(x, time, ...) {
-			if(is.null(colnames(x)))
-				stop("Column names of 'x' have to be named.")
-			return(pile(as.data.frame(x), time, ...))
+# Method for formula
+setMethod("pile", signature(x="formula"),
+		function(x, data, ...) {
+			biomass <- strsplit(as.character(x)[2], "+", fixed=TRUE)[[1]]
+			biomass <- gsub("\\s+", "", biomass)
+			time <- attr(terms(x), "term.labels")
+			return(pile(data, time, biomass))
 		}
 )
---
+++ R/read_sunscan.R
@@ -0,0 +1,143 @@
+# TODO:   Add comment
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+# Reading version 1.x
+Sunscan_Raw_v1 <- function(x) {
+	Head <- gsub("^ *|(?<= ) | *$", "", x, perl=TRUE)
+	Head <- strsplit(Head, " ", fixed=TRUE)
+	Head <- Data <- Head[sapply(Head, length) > 0]
+	Head <- Head[1:3]
+	index.2 <- cumsum(grepl("Ext", Data))
+	Data <- split(Data[index.2 > 0], index.2[index.2 > 0])
+	# Extract section as table
+	Section <- list()
+	for(j in 1:length(Data)) {
+		Head.2 <- Data.2 <- Data[[j]]
+		Head.2 <- Head.2[1:3]##
+		Data.2 <- Data.2[-c(1:3)]##
+		index.3 <- seq(1, length(Data.2), by=5)
+		for(k in index.3) {
+			if(length(Data.2[[k]]) == 7) Data.2[[k]] <- c(Data.2[[k]], "")
+			if(length(Data.2[[k]]) > 8) Data.2[[k]] <- c(Data.2[[k]][1:7],
+						paste(Data.2[[k]][8:length(Data.2[[k]])], collapse=" "))
+			Data.2[[k]] <- c(Data.2[[k]], Data.2[[k+1]], Data.2[[k+2]],
+					Data.2[[k+3]], Data.2[[k+4]])
+		}
+		# Constructing table
+		Data.2 <- Data.2[index.3]
+		Data.2 <- t(sapply(Data.2, cbind))
+		colnames(Data.2) <- c(Head.2[[3]], c(1:64))
+		Data.2[,2] <- colnames(Data.2)[2]
+		colnames(Data.2)[2:3] <- c("plot","repetition")
+		Data.2 <- data.frame(Group=Head.2[[2]][2], ExtSensor=Head.2[[1]][3],
+				Data.2, stringsAsFactors=FALSE, check.names=FALSE)
+		for(k in colnames(Data.2)[!colnames(Data.2) %in% c("ExtSensor","Time",
+						"plot", "repetition","Notes")]) Data.2[,k] <-
+					as.numeric(Data.2[,k])
+		Section[[j]] <- Data.2
+	}
+	if(length(Section) == 1) Section <- Section[[1]] else Section <-
+				do.call(rbind, Section)
+	Section <- data.frame(Title=gsub("Title :", "", paste(Head[[1]],
+							collapse=" ")), Location=gsub("Location :", "",
+					paste(Head[[2]], collapse=" ")), Date=Head[[3]], Section,
+			stringsAsFactors=FALSE, check.names=FALSE)
+	return(Section)
+}
+
+Sunscan_LAI_v1 <- function(x) {
+	Head <- gsub("^ *|(?<= ) | *$", "", x, perl=TRUE)
+	Head <- strsplit(Head, " ", fixed=TRUE)
+	Head <- Data <- Head[sapply(Head, length) > 0]
+	Head <- Head[1:4]
+	index.2 <- cumsum(grepl("Ext", Data))
+	Data <- split(Data[index.2 > 0], index.2[index.2 > 0])
+	# Extract section as table
+	Section <- list()
+	for(j in 1:length(Data)) {
+		Head.2 <- Data.2 <- Data[[j]]
+		Head.2 <- Head.2[1:4]
+		Data.2 <- Data.2[-c(1:4)]
+		# Constructing table
+		for(k in 1:length(Data.2)) {
+			if(length(Data.2[[k]]) == 9) Data.2[[k]] <- c(Data.2[[k]], "")
+			if(length(Data.2[[k]]) > 10) Data.2[[k]] <- c(Data.2[[k]][1:9],
+						paste(Data.2[[k]][10:length(Data.2[[k]])],
+								collapse=" "))
+		}
+		Data.2 <- t(sapply(Data.2, cbind))
+		colnames(Data.2) <- Head.2[[3]]
+		Data.2[,2] <- colnames(Data.2)[2]
+		colnames(Data.2)[2:3] <- c("plot","repetition")
+		colnames(Data.2)[colnames(Data.2) == "Trans-"] <- "Transmitted"
+		colnames(Data.2)[colnames(Data.2) == "Incid-"] <- "Incident"
+		Data.2 <- data.frame(ExtSensor=Head.2[[1]][3], LeafAngle=Head.2[[1]][8],
+				LeafAbsorption=Head.2[[1]][12], Group=Head.2[[2]][2], Data.2,
+				stringsAsFactors=FALSE, check.names=FALSE)
+		for(k in colnames(Data.2)[!colnames(Data.2) %in% c("ExtSensor","Group",
+						"Time","plot","repetition","Notes")]) Data.2[,k] <-
+					as.numeric(Data.2[,k])
+		Section[[j]] <- Data.2
+	}
+	if(length(Section) == 1) Section <- Section[[1]] else Section <-
+				do.call(rbind, Section)
+	Section <- data.frame(Title=gsub("Title :", "", paste(Head[[1]],
+							collapse=" ")),
+			Location=gsub("Location :", "", paste(Head[[2]], collapse=" ")),
+			Latitude=gsub("Latitude :", "", paste(Head[[3]][1:2],
+							collapse=" ")),
+			Longitude=gsub("Longitude :", "", paste(Head[[3]][3:4],
+							collapse=" ")),
+			Date=Head[[4]][1], TimeZone=Head[[4]][5], Section,
+			stringsAsFactors=FALSE, check.names=FALSE)
+	return(Section)
+}
+
+## Reading version 2.x
+Sunscan_Raw_v2 <- function(x) {
+	Data <- strsplit(x, "\t", fixed=TRUE)
+	Head <- Data[1:10]
+	Data <- Data[sapply(Data, length) == 74][-1]
+	Data <- t(sapply(Data, cbind))
+	colnames(Data) <- Head[[10]]
+	Data <- data.frame(Title=Head[[1]][2], Location=Head[[2]][2],
+			Date=Head[[4]][1], ExtSensor=Head[[7]][2], Data,
+			stringsAsFactors=FALSE, check.names=FALSE)
+	Data <- Data[,!colnames(Data) %in% c("V8","V9")]
+	for(k in colnames(Data)[!colnames(Data) %in% c("Title","Location","Date",
+					"ExtSensor","Time","plot","repetition",
+					"Notes")]) Data[,k] <- as.numeric(Data[,k])
+	return(Data)
+}
+
+## The hull function -----------------------------------------------------------
+read_sunscan <- function(file, version=1, collapse=FALSE) {
+	x <- readLines(file, warn=FALSE)
+	index <- cumsum(grepl("Title", x))
+	x <- split(x[index > 0], index[index > 0])
+	Out <- list(Raw=list(), LAI=list())
+	if(version == 1) {
+		for(i in 1:length(x)) {
+			if(grepl("Latitude", x[[i]][3])) {
+				Out[["LAI"]][[length(Out[["LAI"]]) + 1]] <-
+						Sunscan_LAI_v1(x[[i]])
+			} else {
+				Out[["Raw"]][[length(Out[["Raw"]]) + 1]] <-
+						Sunscan_Raw_v1(x[[i]])
+			}
+		}
+	}
+	if(version == 2) {
+		for(i in 1:length(x)) {
+			Out[["Raw"]][[length(Out[["Raw"]]) + 1]] <- Sunscan_Raw_v2(x[[i]])
+		}
+	}
+	if(collapse == TRUE) {
+		Out[["Raw"]] <- do.call(rbind, Out[["Raw"]])
+		Out[["LAI"]] <- do.call(rbind, Out[["LAI"]])
+	}
+	return(Out)
+}
+
