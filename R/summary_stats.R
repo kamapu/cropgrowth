56e9c071bbdd816dbfc42503aeae8f3529ea3e2a
Miguel Alvarez <kamapu78@gmail.com>
2018-06-21 09:43:29 +0200

    Modified function summary_stats

--- R/summary_stats.R
+++ R/summary_stats.R
@@ -3,17 +3,33 @@
 # Author: Miguel Alvarez
 ################################################################################
 
-summary_stats <- function(variable, factor, trend=mean, spread=sd,
-		data=NULL, trend_args=list(), spread_args=list()) {
-	variable <- substitute(variable)
-	variable <- eval(variable, data, parent.frame())
-	factor <- substitute(factor)
-	factor <- eval(factor, data, parent.frame())
-	var_trend <- aggregate(variable ~ factor, data, function(x) do.call(trend,
-						c(list(x=x), trend_args)))
-	var_spread <- aggregate(variable ~ factor, data, function(x) do.call(spread,
-						c(list(x=x), spread_args)))
-	colnames(var_trend)[2] <- "tendency"
-	var_trend$dispersion <- var_spread[,2]
-	return(var_trend)
-}
+# Generic function
+setGeneric("summary_stats",
+		function(x, ...)
+			standardGeneric("summary_stats")
+)
+
+# Method for class 'formula'
+setMethod("summary_stats", signature(x="formula"),
+		function(x, trend=mean, spread=sd, data, trend_args=list(),
+				spread_args=list()) {
+			var_trend <- aggregate(x, data, function(x) do.call(trend,
+								c(list(x=x), trend_args)))
+			var_spread <- aggregate(x, data, function(x) do.call(spread,
+								c(list(x=x), spread_args)))
+			colnames(var_trend)[ncol(var_trend)] <- "tendency"
+			var_trend$dispersion <- var_spread[,ncol(var_trend)]
+			return(var_trend)
+		}
+)
+
+# Method for class 'data.frame'
+setMethod("summary_stats", signature(x="character"),
+		function(x, factors, trend=mean, spread=sd, data, trend_args=list(),
+				spread_args=list()) {
+			x <- as.formula(paste(variable, "~", paste(factors,
+									collapse=" + ")))
+			return(summary_stats(x, trend, spread, data, trend_args,
+							spread_args))
+		}
+)
